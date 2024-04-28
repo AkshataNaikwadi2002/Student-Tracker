911f2d0946258a6562e101344b651bba57add0c9
AkshataNaikwadi2002 <119582223+AkshataNaikwadi2002@users.noreply.github.com>
2024-04-28 22:05:09 +0530

    updated

---
+++ CoreJava.js
@@ -0,0 +1,11 @@
+function CheckCred() {
+	let cnd=true;
+	if(cnd)
+	{
+		open("CoreJava.html");
+	}
+	else
+	{
+		alert("this is Core Java Section")
+	}
+}
---
+++ login.js
@@ -0,0 +1,11 @@
+function CheckCred() {
+	let cnd=true;
+	if(cnd)
+	{
+		open("Home.html");
+	}
+	else
+	{
+		alert("entered credentials are incorrect")
+	}
+}
